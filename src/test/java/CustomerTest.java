20fbec683241452215fcae75254dd03baa2c16be
KwonYongkil <saekil@gmail.com>
2016-05-12 00:07:34 +0900

    Add Test Unit

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,23 +1,52 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class CustomerTest {
-    @Test
-    public void statment() throws Exception {
-        Customer customer = new Customer("Kwon");
-        customer.addRental(new Rental(new Movie("",Movie.CHILDRENS),0));
 
-        assertThat(customer.getName(), is("Kwon"));
-//        assertThat(customer.statment(),
-//                is("Rental Record for Kwon\\n\\t\\t1.5\\nAmount owed is 1.5\\nYou earned 1 frequent renter points"));
+    private Customer customer;
+
+    @Before
+    public void SetUp() {
+        customer = new Customer("Kwon");
     }
 
     @Test
-    public void getName() throws Exception {
-        Customer customer = new Customer("");
-        assertThat(customer.getName(), is(""));
+    public void doRentOneChildrenMovieWithZeroDayStatment() throws Exception {
+        customer.addRental(new Rental(new Movie("", Movie.CHILDRENS), 0));
+        assertThat(customer.statment(),
+                is("Rental Record for Kwon\n\t\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points"));
+    }
+
+    @Test
+    public void doRentOneChildrenMovieOn4DaysStatment() throws Exception {
+        customer.addRental(new Rental(new Movie("", Movie.CHILDRENS), 4));
+        assertThat(customer.statment(),
+                is("Rental Record for Kwon\n\t\t3.0\nAmount owed is 3.0\nYou earned 1 frequent renter points"));
+    }
+
+    @Test
+    public void doRentTwoMovie() throws Exception {
+        customer.addRental(new Rental(new Movie("", Movie.CHILDRENS), 4));
+        customer.addRental(new Rental(new Movie("", Movie.REGULAR), 5));
+        assertThat(customer.statment(),
+                is("Rental Record for Kwon\n\t\t3.0\n\t\t6.5\nAmount owed is 9.5\nYou earned 2 frequent renter points"));
+    }
+
+    @Test
+    public void doRentThreeMovie() throws Exception {
+        customer.addRental(new Rental(new Movie("", Movie.CHILDRENS), 1));
+        customer.addRental(new Rental(new Movie("", Movie.REGULAR), 1));
+        customer.addRental(new Rental(new Movie("", Movie.NEW_RELEASE), 8));
+        assertThat(customer.statment(),
+                is("Rental Record for Kwon\n\t\t1.5\n\t\t2.0\n\t\t24.0\nAmount owed is 27.5\nYou earned 4 frequent renter points"));
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+        assertThat(customer.getName(), is("Kwon"));
     }
 
 }
